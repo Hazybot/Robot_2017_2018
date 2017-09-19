8fab94365562eb1fa4dc9dc103d17102866308bc
MattVil <mattvilain@gmail.com>
2017-09-19 18:44:58 +0200

    FIRST !

---
+++ bluetooth/bluetooth.ino
@@ -0,0 +1,32 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(9, 10);
+void setup() {
+
+  pinMode(13, OUTPUT);
+  
+  mySerial.begin(9600);
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  //Serial.println(mySerial.available());
+  if (mySerial.available()){
+    
+  
+    char c = mySerial.read();
+    Serial.println(c);
+    
+    if(c =='1') {
+      digitalWrite(13, HIGH);
+    }
+    
+    if(c == '0'){
+      digitalWrite(13, LOW);
+    }
+    
+    delay(50);
+  }
+}
+
